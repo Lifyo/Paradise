4d8104119486fdecb81c800872901f2cb1cd3617
Mateusz <shineese22@gmail.com>
2025-02-06 13:13:54 +0100

    created site

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const nav = document.querySelector('.nav-hidden');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav-hidden__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
+const handleNav = () => {
+	nav.classList.toggle('nav-hidden--active');
+
+	navBtnBars.classList.remove('black-bars-color');
+
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav-hidden--active');
+		});
+	});
+
+	handleNavItemsAnimation();
+	deleteAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+
+navBtn.addEventListener('click', handleNav);
+
+const deleteAnimation = () => {
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			allNavItems.forEach((el) => {
+				el.classList.remove('nav-items-animation');
+			});
+		});
+	});
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (
+			!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+window.addEventListener('scroll', handleObserver);
